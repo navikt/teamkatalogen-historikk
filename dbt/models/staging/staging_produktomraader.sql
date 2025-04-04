8a220eebed711a6c59428c2cd47236acc4a6947a
Brynjar Morka Mæhlum <brynjar.morka.mehlum@nav.no>
2025-04-04 07:49:24 +0200

    kommentar om bruk at staging i snapshot

--- dbt/models/staging/staging_produktomraader.sql
+++ dbt/models/staging/staging_produktomraader.sql
@@ -1,6 +1,7 @@
 -- staging_produktomraader
 
-select 
+-- obs! Denne modellen blir brukt av dbt snapshot, så derfor må det være et timestamp
+select
     *,
     cast(last_modified_date as timestamp) as last_modified_timestamp,
 from {{ source('teamkatalogen', 'Produktomraader') }}
